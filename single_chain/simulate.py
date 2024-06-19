357f7c6e9e453f29c0a43368b67dc2b7557ae275
gitesei <giu.tesei@gmail.com>
2024-06-19 14:57:37 +0200

    Single-chain sim slurm and local

--- single_chain/simulate.py
+++ single_chain/simulate.py
@@ -9,22 +9,56 @@ import mdtraj as md
 from argparse import ArgumentParser
 
 parser = ArgumentParser()
-parser.add_argument('--name',nargs='?',const='', type=str)
-parser.add_argument('--cutoff',dest='cutoff',type=float)
+parser.add_argument('--seq_name',nargs='?',required=True)
+parser.add_argument('--path',nargs='?',required=True)
 args = parser.parse_args()
 
 print(hoomd.__file__)
 
-def genParams(r,prot):
-    RT = 8.3145*prot.temp*1e-3
+def xy_spiral_array(n, delta=0, arc=.38, separation=.7):
+    """
+    create points on an Archimedes' spiral
+    with `arc` giving the length of arc between two points
+    and `separation` giving the distance between consecutive
+    turnings
+    """
+    def p2c(r, phi):
+        """
+        polar to cartesian
+        """
+        return (r * np.cos(phi), r * np.sin(phi))
+    r = arc
+    b = separation / (2 * np.pi)
+    phi = float(r) / b
+    coords = []
+    for i in range(n):
+        coords.append(list(p2c(r, phi))+[0])
+        phi += float(arc) / r
+        r = b * phi
+    return np.array(coords)+delta
+
+def gen_init_top(residues,fasta,path,L):
+    N_res = len(fasta)
+    top = md.Topology()
+    chain = top.add_chain()
+    for resname in fasta:
+        residue = top.add_residue(residues.loc[resname,'three'], chain)
+        top.add_atom('CA', element=md.element.carbon, residue=residue)
+    for i in range(N_res-1):
+        top.add_bond(top.atom(i),top.atom(i+1))
+    pos = xy_spiral_array(N_res)
+    t = md.Trajectory(np.array(pos).reshape(N_res,3), top, 0, [L,L,L], [90,90,90])
+    t.save_pdb(path+'/init_top.pdb')
+
+def gen_params(r,seq,temp,ionic):
+    RT = 8.3145*temp*1e-3
     fepsw = lambda T : 5321/T+233.76-0.9297*T+0.1417*1e-2*T*T-0.8292*1e-6*T**3
-    epsw = fepsw(prot.temp)
+    epsw = fepsw(temp)
     lB = 1.6021766**2/(4*np.pi*8.854188*epsw)*6.022*1000/RT
     # Calculate the inverse of the Debye length
-    yukawa_kappa = np.sqrt(8*np.pi*lB*prot.ionic*6.022/10)
-    fasta = prot.fasta.copy()
-    # Set the charge on HIS based on the pH of the protein solution? Not needed if pH=7.4
-    r.loc['H','q'] = 1. / ( 1 + 10**(prot.pH-6) )
+    yukawa_kappa = np.sqrt(8*np.pi*lB*ionic*6.022/10)
+    fasta = list(seq.fasta)
+    r.loc['H','q'] = 1. / ( 1 + 10**(seq.pH-6) )
     r.loc['X'] = r.loc[fasta[0]]
     r.loc['X','q'] = r.loc[fasta[0],'q'] + 1.
     r.loc['X','MW'] = r.loc[fasta[0],'MW'] + 2.
@@ -40,42 +74,40 @@ def genParams(r,prot):
     pairs = np.array(list(itertools.combinations_with_replacement(types,2)))
     return yukawa_kappa, yukawa_eps, types, pairs, fasta, r
 
-def centerDCD(residues,name,prot):
-    top = md.Topology()
-    chain = top.add_chain()
-    for resname in prot.fasta:
-        residue = top.add_residue(residues.loc[resname,'three'], chain)
-        top.add_atom(residues.loc[resname,'three'], element=md.element.carbon, residue=residue)
-    for i in range(len(prot.fasta)-1):
-        top.add_bond(top.atom(i),top.atom(i+1))
-    traj = md.load('{:s}/{:s}.gsd'.format(name,name))[100:]
-    traj.top = top
-    traj = traj.image_molecules(inplace=False, anchor_molecules=[set(traj.top.chain(0).atoms)], make_whole=True)
+def gen_xtc(path):
+    traj = md.load_dcd(path+'/traj.dcd', top=path+'/init_top.pdb')
+    traj.xyz *= 10
+    traj.unitcell_lengths *= 10
     traj.center_coordinates()
-    traj.xyz += traj.unitcell_lengths[0,0]/2
-    print('Number of frames: {:d}'.format(traj.n_frames))
-    traj.save_dcd('{:s}/{:s}.dcd'.format(name,name))
-    traj[0].save_pdb('{:s}/{:s}.pdb'.format(name,name))
+    traj[-1].save_pdb(path+'/top.pdb')
+    #skip first 10 frames
+    traj[10:].save_xtc(path+'/traj.xtc')
 
-def simulate(residues,name,prot,cutoff):
-    hoomd.context.initialize("--mode=cpu --nthreads=1");
+def simulate(residues,sequences,seq_name,path):
+    hoomd.context.initialize("--mode=cpu");
     hoomd.option.set_notice_level(1)
     hoomd.util.quiet_status()
 
-    lj_eps = 4.184*.2
-    RT = 8.3145*prot.temp*1e-3
+    seq = sequences.loc[seq_name]
 
-    yukawa_kappa, yukawa_eps, types, pairs, fasta, residues = genParams(residues,prot)
+    lj_eps = 4.184*.2
+    temp = seq.temp
+    ionic_strength = seq.ionic # M
+    RT = 8.3145*temp*1e-3
+
+    yukawa_kappa, yukawa_eps, types, pairs, fasta, residues = gen_params(residues,seq,temp,ionic_strength)
 
     sigmamap = pd.DataFrame((residues.sigmas.values+residues.sigmas.values.reshape(-1,1))/2,
                             index=residues.sigmas.index,columns=residues.sigmas.index)
     lambdamap = pd.DataFrame((residues.lambdas.values+residues.lambdas.values.reshape(-1,1))/2,
                             index=residues.lambdas.index,columns=residues.lambdas.index)
 
-    N_res = prot.N
-    L = (N_res-1)*.38+4
-    N_save = 3000 if N_res < 100 else int(np.ceil(3e-4*N_res**2)*1000)
-    N_steps = 1100*N_save
+    N_res = seq.N
+    L = 200 if N_res > 500 else (N_res-1)*0.38+4
+    N_save = 7000 if N_res < 150 else int(np.ceil(3e-4*N_res**2)*1000)
+    N_steps = 1010*N_save
+
+    gen_init_top(residues,fasta,path,L)
 
     snapshot = hoomd.data.make_snapshot(N=N_res,
                                 box=hoomd.data.boxdim(Lx=L, Ly=L, Lz=L),
@@ -84,7 +116,7 @@ def simulate(residues,name,prot,cutoff):
 
     snapshot.bonds.resize(N_res-1);
 
-    snapshot.particles.position[:] = [[0,0,(i-N_res/2.)*.38] for i in range(N_res)]
+    snapshot.particles.position[:] = md.load(path+'/init_top.pdb').xyz
     snapshot.particles.typeid[:] = [types.index(a) for a in fasta]
     snapshot.particles.mass[:] = [residues.loc[a].MW for a in fasta]
 
@@ -98,17 +130,17 @@ def simulate(residues,name,prot,cutoff):
 
     nl = hoomd.md.nlist.cell();
 
-    ah = azplugins.pair.ashbaugh(r_cut=cutoff, nlist=nl)
+    ah = azplugins.pair.ashbaugh(r_cut=2.0, nlist=nl)
     yukawa = hoomd.md.pair.yukawa(r_cut=4.0, nlist=nl)
     for a,b in pairs:
-        ah.pair_coeff.set(a, b, lam=lambdamap.loc[a,b], epsilon=lj_eps, sigma=sigmamap.loc[a,b], r_cut=cutoff)
+        ah.pair_coeff.set(a, b, lam=lambdamap.loc[a,b], epsilon=lj_eps, sigma=sigmamap.loc[a,b], r_cut=2.0)
         yukawa.pair_coeff.set(a, b, epsilon=yukawa_eps.loc[a,b], kappa=yukawa_kappa, r_cut=4.)
 
     ah.set_params(mode='shift')
     yukawa.set_params(mode='shift')
     nl.reset_exclusions(exclusions = ['bond'])
 
-    integrator_mode = hoomd.md.integrate.mode_standard(dt=0.005);
+    hoomd.md.integrate.mode_standard(dt=0.005);
     integrator = hoomd.md.integrate.langevin(group=hoomd.group.all(),kT=RT,seed=np.random.randint(100));
 
     for a in types:
@@ -118,21 +150,23 @@ def simulate(residues,name,prot,cutoff):
 
     integrator.disable()
 
-    integrator_mode = hoomd.md.integrate.mode_standard(dt=0.01);
+    hoomd.md.integrate.mode_standard(dt=0.01);
     integrator = hoomd.md.integrate.langevin(group=hoomd.group.all(),kT=RT,seed=np.random.randint(100));
 
     for a in types:
         integrator.set_gamma(a, residues.loc[a].MW/100)
 
-    hoomd.dump.gsd(filename='{:s}/{:s}.gsd'.format(name,name), period=N_save, group=hoomd.group.all(), overwrite=True);
+    hoomd.dump.dcd(filename=path+'/traj.dcd', period=N_save, group=hoomd.group.all(), unwrap_full=True, overwrite=True);
 
     hoomd.run(N_steps)
-    centerDCD(residues,name,prot)
+
+    gen_xtc(path)
 
 residues = pd.read_csv('residues.csv').set_index('one',drop=False)
 
-proteins = pd.read_pickle('proteins.pkl')
+sequences = pd.read_csv('sequences.csv',index_col=0)
+sequences['N'] = sequences.fasta.apply(len)
 
 t0 = time.time()
-simulate(residues,args.name,proteins.loc[args.name],args.cutoff)
+simulate(residues,sequences,args.seq_name,args.path)
 print('Timing Simulation {:.3f}'.format(time.time()-t0))
---
+++ single_chain/submit_slurm.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+from jinja2 import Template
+import pandas as pd
+
+submission = Template("""#!/bin/sh
+#SBATCH --job-name={{seq_name}}
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=2
+#SBATCH --partition=sbinlab_ib
+#SBATCH --mem=3GB
+#SBATCH -t 72:00:00
+#SBATCH -o {{path}}/out
+#SBATCH -e {{path}}/err
+
+source /groups/sbinlab/giulio/.bashrc
+conda activate hoomd
+
+python ./simulate.py --seq_name {{seq_name}} --path {{path}}""")
+
+def initProteins():
+    proteins = pd.DataFrame(columns=['temp','pH','ionic','fasta','N'],dtype=object)
+
+    # DOI: 10.1073/pnas.1322611111
+    fasta_ACTR = """GTQNRPLLRNSLDDLVGPPSNLEGQSDERALLDQLHTLLSNTDATGLEEIDRALGIPELVNQGQALEPKQD""".replace('\n', '')
+
+    proteins.loc['ACTR'] = dict(temp=278,pH=7.4,fasta=fasta_ACTR,ionic=0.2,N=len(fasta_ACTR))
+    return proteins
+
+sequences = initProteins()
+sequences.to_csv('sequences.csv')
+
+parent_dir = './' # or e.g. 'replica_0'
+
+if not os.path.isdir(parent_dir):
+    os.mkdir(parent_dir)
+
+for seq_name in sequences.index:
+    path = f'{parent_dir:s}/{seq_name:s}'
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    with open(f'{seq_name:s}.sh', 'w') as submit:
+        submit.write(submission.render(seq_name=seq_name,path=path))
+    subprocess.run(['sbatch',f'{seq_name:s}.sh'])
