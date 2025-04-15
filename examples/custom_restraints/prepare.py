cba1ce76aac2b95bff6692eca689288caf0a6fd6
Sören von Bülow <soren.bulow@bio.ku.dk>
2025-04-15 10:03:48 +0200

    Custom restraints example

---
+++ examples/custom_restraints/prepare.py
@@ -0,0 +1,86 @@
+import os
+import pandas as pd
+from calvados.cfg import Config, Job, Components
+import subprocess
+import numpy as np
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('--name',nargs='?',required=True,type=str)
+args = parser.parse_args()
+
+cwd = os.getcwd()
+sysname = f'{args.name:s}'
+
+# set the side length of the cubic box
+L = 40
+
+# set the saving interval (number of integration steps)
+N_save = 8000
+
+# set final number of frames to save
+N_frames = 4000
+
+residues_file = f'{cwd}/input/residues_CALVADOS3.csv'
+
+config = Config(
+  # GENERAL
+  sysname = sysname, # name of simulation system
+  box = [L, L, L], # nm
+  temp = 293, # K
+  ionic = 0.19, # molar
+  pH = 7.0,
+  topol = 'center',
+
+  # RUNTIME SETTINGS
+  wfreq = N_save, # dcd writing interval, 1 = 10 fs
+  steps = N_frames*N_save, # number of simulation steps
+  runtime = 0, # overwrites 'steps' keyword if > 0
+  platform = 'CPU', # or CUDA
+  threads = 4,
+  restart = 'checkpoint',
+  frestart = 'restart.chk',
+  verbose = True,
+
+  custom_restraints = True,
+  custom_restraint_type = 'harmonic',
+  fcustom_restraints = f'{cwd}/input/cres.txt',
+)
+
+# PATH
+path = f'{cwd}/{sysname:s}'
+subprocess.run(f'mkdir -p {path}',shell=True)
+subprocess.run(f'mkdir -p data',shell=True)
+
+analyses = f"""
+
+from calvados.analysis import save_conf_prop
+
+save_conf_prop(path="{path:s}",name="{sysname:s}",residues_file="{residues_file:s}",output_path=f"{cwd}/data",start=100,is_idr=False,select='all')
+"""
+
+config.write(path,name='config.yaml',analyses=analyses)
+
+components = Components(
+  # Defaults
+  molecule_type = 'protein',
+  nmol = 1, # number of molecules
+  restraint = True, # apply restraints
+  charge_termini = 'both', # charge N or C or both
+  # INPUT
+  fresidues = residues_file, # residue definitions
+  fdomains = f'{cwd}/input/domains.yaml', # domain definitions (harmonic restraints)
+  pdb_folder = f'{cwd}/input', # directory for pdb and PAE files
+  # RESTRAINTS
+  restraint_type = 'harmonic', # harmonic or go
+  use_com = True, # apply on centers of mass instead of CA
+  colabfold = 1, # PAE format (EBI AF=0, Colabfold=1&2)
+  k_harmonic = 700., # Restraint force constant
+)
+components.add(name=args.name)
+
+components.write(path,name='components.yaml')
+
+# job = Job(envname='calvados-public')
+# job.write(path,config,components)
+# job.submit(path,njobs=1)
